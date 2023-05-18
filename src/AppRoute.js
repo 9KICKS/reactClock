a50117d2903bb88787c33fcb7b55354c5ccfe37b
9KICKS <officialjoshua9@gmail.com>
2023-05-18 10:53:28 +0100

    "updated the Clock"

--- src/AppRoute.js
+++ src/AppRoute.js
@@ -1,10 +1,15 @@
 import Clock from "./pages/dashboard/components/Clock";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function AppRoute() {
   return (
-    <div className="App">
-      <Clock />
-    </div>
+      <div className="App">
+          <Router>
+              <Routes>
+                  <Route path="/" element={<Clock />} />
+              </Routes>
+          </Router>
+      </div>
   );
 }
 
